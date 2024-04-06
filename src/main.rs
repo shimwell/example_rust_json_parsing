35b160ad5ab700f6a3358daf7d2f587dd002b0a0
Jonathan Shimwell <mail@jshimwell.com>
2024-04-06 23:17:38 +0100

    floats

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
-#[tokio::main]
+use serde::{Serialize, Deserialize};
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, Serialize, Deserialize)]
+struct Todo {
+    energy: f32,
+    cross_section: f32,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    // Receive type-checked JSON
+    
+    let todos: Vec<Todo> = reqwest::Client::new()
+        .get("https://raw.githubusercontent.com/shimwell/example_rust_json_parsing/main/example.json")
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    println!("{:#?}", todos);
+
+    Ok(())
 }
